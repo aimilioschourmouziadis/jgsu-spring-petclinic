c3602b1151535b84a3fbeacfbfd0f4b2d6ab8500
aimilioschourmouziadis <aimilios.chourmouziadis@inmarsat.com>
2022-11-24 11:07:22 +0000

    Changed checkout

--- ide/pipeline.groovy
+++ ide/pipeline.groovy
@@ -4,7 +4,8 @@ pipeline {
     stages {
         stage('Checkout') {
             steps {
-                git url: 'https://github.com/g0t4/jgsu-spring-petclinic.git', branch: 'main'
+                git url: 'https://github.com/aimilioschourmouziadis/jgsu-spring-petclinic.git', 
+                branch: 'main'
             }            
         }
         stage('Build') {
